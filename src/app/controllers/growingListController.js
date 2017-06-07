bea5befacbcdb19f276c141d89582cd7b34c0e7b
Tom McClean <tommcclean@hotmail.co.uk>
2017-06-07 23:14:02 +0100

    Further Improvements to the Growling List Controller

--- src/app/controllers/growingListController.js
+++ src/app/controllers/growingListController.js
@@ -7,17 +7,19 @@
         var self = this;
 
         self.items = [
-            { headline: 'Item 1', description: "This is the first item", body: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. " },
-            { headline: 'Item 2', description: "This is the second item", body: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. " },
-            { headline: 'Item 3', description: "This is the third item", body: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. " },
+            { headline: 'Item 1', body: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. " },
+            { headline: 'Item 2', body: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. " },
+            { headline: 'Item 3', body: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. " },
         ];
 
         self.headline = "";
-        self.description = "";
         self.body = "";
 
-        self.submit = function() {
-            self.items.push({ headline: self.headline, description: self.description, body: self.body })
+        self.submit = function () {
+            self.items.push({ headline: self.headline, body: self.body })
+
+            self.headline = "";
+            self.body = "";
         };
 
         return self;
